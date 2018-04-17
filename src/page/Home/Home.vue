8349fb2ddd5583b4029df6f2196ea1f12054c699
user <user@my.local>
2018-04-17 17:02:06 +0800

    [循环 条件 绑定事件(警告太多了！！！！！)]

--- src/page/Home/Home.vue
+++ src/page/Home/Home.vue
@@ -3,15 +3,37 @@
     <div>demo1数据双向绑定</div>
     <div>{{msg}}</div>
     <input type="text" v-model='msg'>
+    <div v-bind:title='tip' >
+      demo2 鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </div>
+    <div>demo3 循环</div>
+    <div  v-for="item in todos" :key="item.text">
+      {{item.text}}
+    </div>
+    <div>demo4 条件</div>
+    <div v-if='flag'>条件</div>
+    <div>demo5 点击事件</div>
+    <div v-on:click='press' >按钮</div>
+    <div @click='press' >按钮</div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'Home',
   data () {
     return {
-      msg: 'hello home'
+      msg: 'hello home',
+      tip: '提示',
+      todos: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' }
+      ],
+      flag: true
+    }
+  },
+  methods: {
+    press (event) {
+      console.log(this)
     }
   }
 }
